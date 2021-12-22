ef23070e0422b054cf09b49acb8a9f3d3c6f1978
Paria <paria.he84@gmail.com>
2021-12-22 22:24:13 +0100

    updated to get data from the URL

--- src/components/App.js
+++ src/components/App.js
@@ -7,28 +7,46 @@ import Header from './Header';
 
 
 class App extends React.Component{
-    state = {
-        articles: [],
-        setArticles:[],
-      };
 
-    async componentDidMount() {
-        // GET request with async/await
-        const response = await dagBladet.get(`/aller-structure-task/test_data.json`);
-        const data = await response.data[0];
-        this.setState({ articles: data })
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
     }
+    
+    callAPI() {
+        fetch("http://localhost:9000/appAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => console.warn(err));
+    }
+    
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    // state = {
+    //     articles: [],
+    //     setArticles:[],
+    //   };
+
+    // async componentDidMount() {
+    //     // GET request with async/await
+    //     const response = await dagBladet.get(`/aller-structure-task/test_data.json`);
+    //     const data = await response.data[0];
+    //     this.setState({ articles: data })
+    // }
     
     render(){
         return (
         <div>
-            <BrowserRouter>
+            <p className="App-intro">;{this.state.apiResponse}</p>
+            {/* <BrowserRouter>
             <Header/>
                 <Routes>
                     <Route path="/"  element={<Articles articles={this.state.articles}/>}  />
                     <Route path="/ArticlesList&height=200&width=200"  element={<ArticlesList articles={this.state.articles}/>}/>
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter> */}
         </div>
         )
     }
